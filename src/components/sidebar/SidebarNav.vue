166b44642fbdd6b0446b6aa96a410fa44bfae7c7
Anton Reshetov <reshetov.art@gmail.com>
2019-04-23 16:40:51 +0300

    feat: add sidebar nav

---
+++ src/components/sidebar/SidebarNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sidebar-nav">
+    <SidebarNavGroup
+      v-for="(i, index) in data"
+      :key="index"
+      :items="i"
+    />
+  </div>
+</template>
+
+<script>
+
+import SidebarNavGroup from './SidebarNavGroup'
+export default {
+  name: 'SidebarNav',
+
+  components: {
+    SidebarNavGroup
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
